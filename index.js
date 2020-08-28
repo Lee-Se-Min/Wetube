275b1732354d68f787a8b6125fc0398bfadd6e40
leesemin <leesemin86@gmail.com>
2020-08-28 10:49:18 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ function handleListen() {
   console.log(`[V] Listen on : http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  res.send("Hello from Home");
+}
+function handleProfile(req, res) {
+  res.send("Your on my Profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListen);
