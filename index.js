a3693eddf6344264242256d9ce165d727ac2e50d
leesemin <leesemin86@gmail.com>
2020-08-28 12:11:58 +0900

    #2.5 ES6 on NodeJS using Babel and install nodemon

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
 const PORT = 4000;
 
-function handleListen() {
+const handleListen = () => {
   console.log(`[V] Listen on : http://localhost:${PORT}`);
-}
+};
 
-function handleHome(req, res) {
+const handleHome = (req, res) => {
   res.send("Hello from Home");
-}
-function handleProfile(req, res) {
+};
+const handleProfile = (req, res) => {
   res.send("Your on my Profile");
-}
+};
 
 app.get("/", handleHome);
 
